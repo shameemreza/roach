26a3057965e2ffdeb01bd97ab06a643afd89c26b
Shameem Reza <codechef@yahoo.com>
2022-11-29 21:04:26 +0600

    Local Fonts Support :tada:

--- template-parts/none/content-none.php
+++ template-parts/none/content-none.php
@@ -1,52 +1,55 @@
-<?php if ((is_single()) || (is_page())) : ?>
+<?php if ( (is_single()) || (is_page()) ) : ?>
 
-  <p class="p404"><?php _e('Nothing found', 'roach'); ?></p>
-
-<?php endif; ?>
-
-<?php if ((is_front_page()) || (is_tag()) || (is_category()) || (is_author())) : ?>
-
-  <p class="p404"><?php _e('There are no published articles yet.', 'roach'); ?></p>
+<p class="p404"><?php _e('Nothing found', 'roach'); ?></p>
 
 <?php endif; ?>
 
-<?php if (is_search()) : ?>
+<?php if ( (is_front_page()) || (is_tag()) || (is_category()) || (is_author()) ) : ?>
 
-  <p class="p404"><?php _e('We have not found results for your search', 'roach'); ?> <span><?php echo get_search_query() ?></span>.</p>
-
-<?php endif; ?>
-
-<?php if (is_404()) : ?>
-
-  <p class="p404"><?php _e('Nothing found', 'roach'); ?>.</p>
+<p class="p404"><?php _e('There are no published articles yet.','roach'); ?></p>
 
 <?php endif; ?>
 
-<?php if ((is_search()) || (is_404())) : ?>
+<?php if(is_search()) : ?>
 
-  <p><?php _e('Other items that might interest you', 'roach'); ?></p>
+<p class="p404"><?php _e('We have not found results for your search', 'roach'); ?> <span><?php echo get_search_query() ?></span>.</p>
 
-  <div class="content-area">
+<?php echo do_shortcode('[roach_search]'); ?>
 
-    <?php
+<?php endif; ?>
 
-    global $post;
+<?php if(is_404()) : ?>
 
-    $last_posts = get_posts(array('showposts' => 6));
+<p class="p404"><?php _e('Nothing found', 'roach'); ?>.</p>
 
-    foreach ($last_posts as $post) :
+<?php endif; ?>
 
-      setup_postdata($post);
+<?php if ( (is_search()) || (is_404()) ) : ?>
 
-      get_columns();
+<p><?php _e('Other items that might interest you', 'roach'); ?></p>
 
-      get_template_part('template-parts/content/content', 'loop');
+<div class="content-area">
 
-    endforeach;
-    wp_reset_postdata();
+<?php 
 
-    ?>
+global $post;
+  
+$cols = get_theme_mod('roach_columns');
+      
+$last_posts = get_posts(array('showposts' => $cols, 'orderby' => 'rand'));
 
-  </div>
+foreach ( $last_posts as $post ) :
 
+setup_postdata( $post );
+
+get_columns();
+
+get_template_part('template-parts/content/content', 'loop');
+
+endforeach; wp_reset_postdata();
+  
+?>
+  
+</div>
+  
 <?php endif; ?>
